e2942403b27c1d361087219168d1392a584809b4
Jami <jamischwarzwalder@gmail.com>
2016-02-15 20:31:43 -0800

    Fixing an error

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ carousel = (function(){
 
 	var box = document.querySelector('.carouselbox');
 	var next = box.querySelector('.next');
-	var previous = box.querySelector('.prev');
+	var prev = box.querySelector('.prev');
 	var items = box.querySelectorAll('.content li');
 
 	next.addEventListener('click', function(){
