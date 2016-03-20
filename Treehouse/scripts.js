974b6d990ddba45fbd70cb8aaf117bb6196e40d5
Jami <jamischwarzwalder@gmail.com>
2016-03-19 20:33:57 -0700

    debugging some code

---
+++ Treehouse/scripts.js
@@ -0,0 +1,6 @@
+console.log('Begin Session');
+alert("Help me fix this program!");
+alert("Can you get this message to appear?");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("<p>I\'m practicing \'debugging\'.</p>");
+console.log('End Session');
